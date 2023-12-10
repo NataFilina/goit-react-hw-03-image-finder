a9b1027f56f5a549703c50269a88e046c9e0ea00
NataFilina <filinanatash@gmail.com>
2023-12-10 18:21:03 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,88 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { PureComponent } from 'react';
+import { Triangle } from 'react-loader-spinner';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import * as ImageService from '../service/images';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { Button } from './Button/Button';
+
+export class App extends PureComponent {
+  state = {
+    searchName: '',
+    page: 1,
+    images: [],
+    isLoading: false,
+    error: null,
+    isEmpty: false,
+    isVisible: false,
+    // showModal: false,
+    // tags: '',
+    // largeImageURL: '',
+  };
+  handlerOnSubmit = value => {
+    this.setState({
+      searchName: value,
+      page: 1,
+      images: [],
+      error: null,
+      isEmpty: false,
+    });
+  };
+  getPhotos = async (searchName, page) => {
+    if (!searchName) return;
+    this.setState({ isLoading: true });
+    try {
+      const { hits, total } = await ImageService.getImages(searchName, page);
+      if (hits.length === 0) {
+        this.setState({ isEmpty: true });
+      }
+      this.setState(prevState => ({
+        images: [...prevState.images, ...hits],
+        isVisible: hits.length < Math.ceil(total / 12),
+      }));
+    } catch (error) {
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+  componentDidUpdate(prevProps, prevState) {
+    const { searchName, page } = this.state;
+    if (prevState.searchName !== searchName || prevState.page !== page) {
+      this.getPhotos(searchName, page);
+    }
+  }
+  onLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1, isLoading: true }));
+  };
+
+  render() {
+    const { images, isVisible, isEmpty, error, isLoading, largeImageURL, tag } =
+      this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.handlerOnSubmit} />
+        {isEmpty && <h2>Sorry, there are no images...</h2>}
+        {error && <h2>Something went wrong - {error}</h2>}
+
+        <ImageGallery children={<ImageGalleryItem images={images} />} />
+        {isVisible && !isLoading && images.length > 0 && (
+          <Button
+            onClick={this.onLoadMore}
+            children={isLoading ? 'Loading' : 'Load more'}
+          />
+        )}
+        {isLoading && (
+          <Triangle
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="triangle-loading"
+            wrapperStyle={{ marginLeft: '50%' }}
+            wrapperClassName=""
+            visible={true}
+          />
+        )}
+      </>
+    );
+  }
+}
